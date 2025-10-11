ed951fbeda3fab3dfa1a2c7c1b10419d7348c0f2
Guadalupe <ariasperaltag@gmail.com>
2025-10-11 00:02:56 -0400

    cambios

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const params = new URLSearchParams(window.location.search);
-    const userData = document.getElementById('user-data');
+    const userData = document.getElementById('data-user');
     for (const [key, value] of params.entries()) {
         const li = document.createElement('li');
         li.textContent = `${key}: ${value}`;
